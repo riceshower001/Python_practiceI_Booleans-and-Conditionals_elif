ed4f63dff08865a0ff684d67831585bdc054e736
riceshower001 <125946802+riceshower001@users.noreply.github.com>
2023-03-04 18:07:49 +0900

    Add files via upload

---
+++ Python_practiceI_Booleans-and-Conditionals_elif.py
@@ -0,0 +1,10 @@
+money = 100
+apple_price = 100
+
+if money > apple_price:
+    print('You can buy apples.')
+# If the values of the variables are equal, print 'You can buy apples, but you have no money.'.
+elif money == apple_price:
+  print('You can buy apples, but you have no money.')
+else:
+    print('You do not have enough money.')
